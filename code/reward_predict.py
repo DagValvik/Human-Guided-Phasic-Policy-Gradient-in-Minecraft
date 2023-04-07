dfdbbafa52a42d7150e178b92c3aef0ca4e9114a
Dag Hermann Valvik <31191858+DagValvik@users.noreply.github.com>
2023-04-07 09:50:40 +0200

    Reward predictor draft

--- code/reward_predict.py
+++ code/reward_predict.py
@@ -1,75 +1,71 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 
 
-class CoreNetwork(nn.Module):
-    def __init__(self, dropout, batchnorm):
-        super(CoreNetwork, self).__init__()
-        self.batchnorm = batchnorm
-        self.dropout = dropout
+class CustomCNN(nn.Module):
+    def __init__(self, input_channels, batchnorm=True, dropout=0.5):
+        super(CustomCNN, self).__init__()
+        self.feature_extractor = self.create_feature_extractor(
+            input_channels, batchnorm, dropout
+        )
+        self.classifier = self.create_classifier()
 
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=7, stride=3),
-            nn.LeakyReLU(0.01),
+    def create_feature_extractor(self, input_channels, batchnorm, dropout):
+        layers = [
+            nn.Conv2d(input_channels, 16, kernel_size=7, stride=3, padding=3),
             nn.BatchNorm2d(16) if batchnorm else nn.Identity(),
-            nn.Dropout(dropout),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(16, 16, kernel_size=5, stride=2),
-            nn.LeakyReLU(0.01),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(dropout) if dropout else nn.Identity(),
+            nn.Conv2d(16, 16, kernel_size=5, stride=2, padding=2),
             nn.BatchNorm2d(16) if batchnorm else nn.Identity(),
-            nn.Dropout(dropout),
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(16, 16, kernel_size=3, stride=1),
-            nn.LeakyReLU(0.01),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(dropout) if dropout else nn.Identity(),
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(16) if batchnorm else nn.Identity(),
-            nn.Dropout(dropout),
-        )
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(16, 16, kernel_size=3, stride=1),
-            nn.LeakyReLU(0.01),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(dropout) if dropout else nn.Identity(),
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(16) if batchnorm else nn.Identity(),
-        )
+            nn.ReLU(inplace=True),
+        ]
+        return nn.Sequential(*layers)
 
-        # Update the input size for the first fully connected layer
-        self.fc1 = nn.Sequential(
-            nn.Linear(
-                33600, 64
-            ),  # You may need to adjust this value based on the output size of the last convolutional layer
-            nn.ReLU(),
-        )
-        self.fc2 = nn.Linear(64, 1)
+    def create_classifier(self):
+        layers = [
+            nn.Linear(16 * 45 * 80, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 1),
+        ]
+        return nn.Sequential(*layers)
 
-    def forward(self, s, training):
-        x = s / 255.0
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-
+    def forward(self, x):
+        x = x.float() / 255.0
+        x = self.feature_extractor(x)
         x = x.view(x.size(0), -1)
-
-        x = self.fc1(x)
-        x = self.fc2(x)
-
+        x = self.classifier(x)
         return x.squeeze()
 
 
+# # For single RGB image
+# net_single = CustomCNN(input_channels=3)
+# # For stack of 5 RGB images
+# net_stacked = CustomCNN(input_channels=3*5)
+
+
 class RewardPredictorNetwork(nn.Module):
-    def __init__(self, core_network, dropout, batchnorm, lr):
+    def __init__(self, input_channels, dropout, batchnorm, lr):
         super(RewardPredictorNetwork, self).__init__()
-        self.core_network = core_network(dropout, batchnorm)
+        self.core_network = CustomCNN(input_channels=input_channels, dropout=dropout, batchnorm=batchnorm)
         self.loss_fn = nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.core_network.parameters(), lr=lr)
 
     def forward(self, s1, s2, pref, training):
-        s1_unrolled = s1.view(-1, 84, 84, 4)
-        s2_unrolled = s2.view(-1, 84, 84, 4)
+        self.core_network.train(training)
 
-        _r1 = self.core_network(s1_unrolled, training)
-        _r2 = self.core_network(s2_unrolled, training)
+        _r1 = self.core_network(s1)
+        _r2 = self.core_network(s2)
 
         r1 = _r1.view(s1.size(0), s1.size(1))
         r2 = _r2.view(s2.size(0), s2.size(1))
@@ -97,3 +93,10 @@ class RewardPredictorNetwork(nn.Module):
         loss.backward()
         self.optimizer.step()
         return r1, r2, rs1, rs2, pred, accuracy, loss
+
+
+# # For a single RGB image
+# reward_predictor_single = RewardPredictorNetwork(input_channels=3, dropout=0.5, batchnorm=True, lr=0.001)
+
+# # For a stack of 5 RGB images
+# reward_predictor_stacked = RewardPredictorNetwork(input_channels=3*5, dropout=0.5, batchnorm=True, lr=0.001)
